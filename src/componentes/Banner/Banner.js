4a7f1063fbb92b82f4f522c94623aaf969aeb52f
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2023-02-14 08:05:04 -0300

    Criado Banner

---
+++ src/componentes/Banner/Banner.js
@@ -0,0 +1,12 @@
+import './Banner.css'
+
+function Banner(){
+    // JSX (parece html, mas n é)
+    return (
+        <header className='banner'>
+            <img src="/imagens/banner.png" alt="Banner Principal"/>
+        </header>
+    )
+}
+
+export default Banner
