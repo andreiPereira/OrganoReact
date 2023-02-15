3100028a46eba8084093521fb92d58d247e66e16
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2023-02-15 17:26:25 -0300

    Ajustando Erros

--- src/App.js
+++ src/App.js
@@ -41,8 +41,8 @@ function App() {
   const [colaboradores, setColaboradores] = useState([])
 
   const aoNovoColaboradorAdicionado = (colaborador) => {
+    console.log(colaborador)
     setColaboradores([...colaboradores, colaborador])
-    console.log(colaboradores)
   }
  
   return (
