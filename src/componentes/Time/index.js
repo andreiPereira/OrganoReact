1426fd0bf271e0eae4b95ca9cbd2f2147756a49f
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2023-02-15 17:05:30 -0300

    Limpando tela  criando cards

--- src/componentes/Time/index.js
+++ src/componentes/Time/index.js
@@ -5,12 +5,14 @@ const Time = (props) => {
     const css = {backgroundColor:props.corSecundaria}
     const cssh3 = {borderColor:props.corPrimaria }
     return(
-        <section className='time' style={css}>
-            <h3 style={cssh3}>{props.nome}</h3>
-            <div className='colaboradores'>
-                {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)}
-            </div>
-        </section>
+        (props.colaboradores.length > 0) ?
+            <section className='time' style={css}>
+                <h3 style={cssh3}>{props.nome}</h3>
+                <div className='colaboradores'>
+                    {props.colaboradores.map(colaborador => <Colaborador nome={colaborador.nome} cargo={colaborador.cargo} imagem={colaborador.imagem} />)}
+                </div>
+            </section>
+        :''
     )
 }
 
