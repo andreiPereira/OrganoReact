915a9fc3618f5efaae8699c6f9127536519ee0b6
Andrei Pereira <48697839+andreiPereira@users.noreply.github.com>
2023-02-15 14:54:51 -0300

    Criado Colaborador

---
+++ src/componentes/Colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+    return (
+        <div className='colaborador'> 
+            <div className='cabecalho'>
+                <img src="https://github.com/andreiPereira.png" alt="imagem" />
+            </div>
+            <div className='rodape'>
+            <h4>Andrei</h4>
+                    <h5>Colaborador</h5>
+            </div>
+        </div>
+    )
+}
+
+export default Colaborador
